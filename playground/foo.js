338a6dcabf05957369b5169217628d1580ab561f
yuzhanglong <yuzl1123@163.com>
2021-06-05 15:32:59 +0800

    chore: add playground

---
+++ playground/foo.js
@@ -0,0 +1,9 @@
+const sayHello = () => {
+  console.log('hello world!')
+}
+
+const sum = (a, b) => {
+  return a + b
+}
+
+export { sayHello, sum }
---
+++ playground/main.js
@@ -0,0 +1,4 @@
+import { sayHello, sum } from './foo'
+
+sayHello()
+console.log(sum(1, 2))
