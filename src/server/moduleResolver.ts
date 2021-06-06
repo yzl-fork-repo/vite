449d5a6aa0aa154feeb36c903688a4a65b161908
yuzhanglong <yuzl1123@163.com>
2021-06-06 10:09:13 +0800

    chore: remove useless comments

--- src/server/moduleResolver.ts
+++ src/server/moduleResolver.ts
@@ -2,23 +2,10 @@ import path from 'path'
 import { runCommand, sendJSStream } from './utils'
 import { ServerResponse } from 'http'
 
-// const fileToIdMap = new Map()
 const moduleNameToCachePathMap = new Map()
 
 export function resolveModule(id: string, cwd: string, res: ServerResponse) {
   let modulePath: string
-  // let sourceMapPath: string | undefined = undefined
-  // // TODO support custom imports map e.g. for snowpack web_modules
-  //
-  // if (id.endsWith('.map')) {
-  //   sourceMapPath = id
-  //   id = fileToIdMap.get(id.replace(/\.map$/, ''))
-  //   if (!id) {
-  //     res.statusCode = 404
-  //     res.end()
-  //     return
-  //   }
-  // }
 
   // fallback to node resolve
   try {
@@ -40,17 +27,6 @@ export function resolveModule(id: string, cwd: string, res: ServerResponse) {
     } else {
       modulePath = moduleNameToCachePathMap.get(moduleBaseName)
     }
-
-    // modulePath = resolve(cwd, `${id}/package.json`)
-    //
-    // // module resolved, try to locate its "module" entry
-    // const pkg = require(modulePath)
-    // modulePath = path.join(path.dirname(modulePath), pkg.module || pkg.main)
-    // fileToIdMap.set(path.basename(modulePath), id)
-    // // this is a source map request.
-    // if (sourceMapPath) {
-    //   modulePath = path.join(path.dirname(modulePath), sourceMapPath)
-    // }
 
     sendJSStream(res, modulePath)
   } catch (e) {
